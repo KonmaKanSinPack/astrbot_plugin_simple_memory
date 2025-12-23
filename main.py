e658b48a7120787a7b2cd63b44a74a02b2bef191
KonmaKanSinPack <saierpipi@gmail.com>
2025-12-23 17:53:19 +0800

    1

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
+    @filter.command("memory")
+    async def gen_mem(self, event: AstrMessageEvent):
         """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
+        user_name = event.get_sender_name() #获取用户昵称
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
+        mem_prompt = "请你根据当前的聊天记录以及，生成一份简短的记忆摘要，方便后续的对话使用。"
+
+        with open(f"memory_{user_name}.txt", 'r') as file:
+            cur_mem = file.read()
+            print(cur_mem)
+
+        with open(f"memory_{user_name}.txt", "w", encoding="utf-8") as f:
+            f.write(f"用户 {user_name} 说: {message_str}\n")
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
     async def terminate(self):
