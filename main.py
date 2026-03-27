fe1fdc625b47fce2909057a6ec8581d89a838f7c
KonmaKanSinPack <saierpipi@gmail.com>
2026-03-27 21:33:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -295,6 +295,7 @@ class SimpleMemoryPlugin(Star):
                                 ) -> MessageEventResult:
 
         '''增加/更新/删除自己的一条记忆。
+        当需要修改当前看不到的记忆时，请先调用search_memory_by_name来确认记忆的主体和内容，以确保正确修改记忆。
         大模型可以调用这个工具来修改记忆，调用时请按顺序确保提供正确的参数。
         当大模型需要增加或更新记忆时，必须提供 memory_type=core_memory|long_term|medium_term、action_type=upsert|delete、memory_id（可以是空字符串或null，系统会自动生成唯一 ID）、content（记忆内容）等参数，category、importance、expires_at、subject_id 可选。
         当大模型需要删除记忆时，必须提供 memory_type、action_type=delete、memory_id 参数，其他参数不提供。
